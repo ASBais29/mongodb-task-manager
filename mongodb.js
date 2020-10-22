d142a16dd076981305c518e16f8eb3507e0cfa41
anshuman <bais.anshumansingh@gmail.com>
2020-10-22 22:45:55 +0530

    Code cleaned

--- mongodb.js
+++ mongodb.js
@@ -9,56 +9,8 @@ const databaseName='task-manager'
          return console.log('Unable to connect to database')
      }
      const db=client.db(databaseName)
-    //  db.collection('users').insertOne({
-    //      name: 'Anshuman Singh Bais',
-    //      age:20
-    //  },(error,result)=>{
-    //      if(error)
-    //      {
-    //          return console.log('User undefined')
-    //      }
-    //      console.log(result.connection)
-    //  })
-
-//   db.collection('users').insertMany([
-//       {
-//         name: 'Anshuman Singh Bais',
-//         age:20
-//       },
-//       {
-//         name: 'Sam Curran',
-//        age:25
-//       }
-//     ],(error,result)=>{
-//     if(error)
-//          {
-//              return console.log('User undefined')
-//          }
-//          console.log(result.ops)
-//   })
-
-// db.collection('Tasks').insertMany([
-//     {
-//       description: 'Study',
-//       completed:true
-//     },
-//     {
-//         description: 'Play',
-//         completed:true
-//       },
-//       {
-//         description: 'Cry',
-//         completed:false
-//       }
-//   ],(error,result)=>{
-//   if(error)
-//        {
-//            return console.log('User undefined')
-//        }
-//        console.log(result.ops)
-// })
 db.collection('Tasks').deleteOne({
-  description: 'Fuck'
+  description: 'Run'
 }).then((result)=>{
   console.log(result.modifiedCount)
 }).catch((error)=>{
